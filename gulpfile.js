0931166dde32e0f656f3376c1e313207e0021cb4
Redfield <wesleycrz@hotmail.com>
2017-10-26 17:07:04 -0200

    montando a estrutura

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,29 @@ gulp.task('css', function(){
 
 gulp.task('js', function(){
     gulp.src('./resource/js/*.js')
-    .pipe(gulp.dest('./public/js'))
+    .pipe(gulp.dest('./public/js'));
+
+    gulp.src('./bower_components/clamp-js/clamp.js')
+    .pipe(gulp.dest('./public/js'));
+
+    gulp.src('./bower_components/jquery/dist/jquery.js')
+    .pipe(gulp.dest('./public/js'));
 });
 
+gulp.task('img', function(){
+    gulp.src('./resource/images/*.jpg')
+    .pipe(gulp.dest('./public/images'))
+});
+
+gulp.task('html', function(){
+    gulp.src('./resource/*.html')
+    .pipe(gulp.dest('./public'))
+});
+
+
 gulp.task('watch-dev', function(){
-    gulp.start('css', 'js');
+    gulp.start('css', 'js', 'img', 'html');
     gulp.watch('./resource/**', [
-        'css', 'js'
+        'css', 'js', 'html'
     ]);
 });
--- resource/js/script.js
+++ resource/js/script.js
@@ -1,3 +1,54 @@
 window.onload = function(){
-//    alert('ok');
-}
+    var http = new XMLHttpRequest();
+    var json;
+    http.onreadystatechange = function(){
+        if(http.readyState == 4 && http.status == 200){
+            json = JSON.parse(this.responseText);
+//            console.log(json);
+            var conteudo = document.getElementById('product-shelf');
+            conteudo.innerHTML = "<div class='wrapper-crossproducts>'"
+            for(var i = 0; i < json[0].data.recommendation.length; i++){
+                conteudo.innerHTML +=
+                                "<div class='box-product f-left'>" +
+                                "<img src=" + json[0].data.recommendation[i].imageName + " />" +
+                                "<div class='wrapper-info-product' > " +
+                                "<div class='divh-product-name'><p>" + json[0].data.recommendation[i].name + "</p> </div>" +
+                                "<p> por: " + json[0].data.recommendation[i].price + "</p>" +
+                                "<p>" + json[0].data.recommendation[i].productInfo.paymentConditions + "</p>" +
+                                "</div> <!--wrapper-info-product -->" +
+                                "</div>";
+            }
+            conteudo.innerHTML += "</div>"
+        }
+    }
+    http.open("GET", "../service/products.json", true);
+    http.send();
+
+
+    //Clamp
+//    var item = $('.p3');
+//    $clamp(item[0], {
+//      clamp: 1
+//    });
+
+
+};
+
+$(function(){
+//    var item = $('.divh-product-name p');
+//    $clamp(item[0], {
+//      clamp: 1
+//    });
+
+    var p = $('.divh-product-name p');
+    var divh = $('.divh-product-name').height();
+    while ($(p).outerHeight() > divh) {
+        if($(p).height() > divh){
+            $(p).text(function (index, text) {
+                return text.replace(/\W*\s(\S)*$/, '...');
+            });
+        }else{
+            return null;
+        }
+    }
+})
